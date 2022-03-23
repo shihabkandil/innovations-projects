2c0536cbd087b9688e5b12a7005da94617198ccc
Shihab Kandil <jupitar.orbit01@gmail.com>
2022-03-23 22:10:22 +0200

    Login authentication

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -36,19 +36,25 @@
 <!-- Navbar Start -->
 <nav class="navbar navbar-expand-lg bg-white navbar-light shadow sticky-top p-0">
         <a href="index.html" class="navbar-brand d-flex align-items-center px-4 px-lg-5">
-            <h2 class="m-0 text-primary"><i class="fa fa-book me-3"></i>eLEARNING</h2>
+            <h2 class="m-0 text-primary"><i class="fa fa-book me-3"></i>Innovations Projects</h2>
         </a>
         <button type="button" class="navbar-toggler me-4" data-bs-toggle="collapse" data-bs-target="#navbarCollapse">
             <span class="navbar-toggler-icon"></span>
         </button>
         <div class="collapse navbar-collapse" id="navbarCollapse">
             <div class="navbar-nav ms-auto p-4 p-lg-0">
-                <a href="/home" class="nav-item nav-link">Home</a>
-                <a href="/browse_courses" class="nav-item nav-link">Courses</a>
-                <a href="/about" class="nav-item nav-link">About</a>
-                <a href="/contact" class="nav-item nav-link">Contact</a>            
-                <a href="/login" class="nav-item nav-link">Login</a>
-            <a href="/register" class="btn btn-warning py-4 px-lg-5 d-none d-lg-block">Join Now<i class="fa fa-arrow-right ms-3"></i></a>
+                <a href="/home" class="nav-item nav-link">{{ __('Home') }}</a>
+                <a href="/browse_courses" class="nav-item nav-link">{{ __('Courses') }}</a>
+                <a href="/about" class="nav-item nav-link">{{ __('About') }}</a>
+                <a href="/contact" class="nav-item nav-link">{{ __('Contact') }}</a>
+                @auth
+                <a href="/logout" class="nav-item nav-link">{{ __('Log Out') }}</a>
+                @endauth
+
+            @guest
+                <a href="/login" class="nav-item nav-link">{{ __('Login') }}</a>
+                <a href="/register" class="btn btn-warning py-4 px-lg-5 d-none d-lg-block">{{ __('Join us') }}<i class="fa fa-arrow-right ms-3"></i></a>
+            @endguest
         </div>
       
     </nav>
