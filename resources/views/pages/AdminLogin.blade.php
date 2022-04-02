adaa54f420fb662ba7b823dc703fdac61b7e322a
hashim <hashim_becool@hotmail.com>
2022-04-03 00:39:43 +0200

    log in blue color

--- resources/views/pages/AdminLogin.blade.php
+++ resources/views/pages/AdminLogin.blade.php
@@ -1,4 +1,4 @@
-@extends('layouts.app')
+
 <header>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 </header>
@@ -6,7 +6,7 @@
 
 <title>Admin Login</title>
 
-@section('content')
+
 
 <div class="container-xxl py-5">
         <div class="container text-center">
@@ -15,10 +15,6 @@
                 <h5 class="mb-5">{{ __('Administrator') }}  </h5>
   
 
-
-
-
-                
             </div>
             <form method="POST">
                 @csrf
@@ -49,16 +45,13 @@
                                 </div>
                             </div>
                             <div class="col-12 " >
-                                <button type="submit" class="btn btn-warning w-100 py-3" >{{ __('Login') }}</button>
+                                <button type="submit" class="btn  w-100 py-3" style = "background:#0d6efd ; color :white"  >{{ __('Login') }}</button>
                             </div>
                         </div>
                     </form>
-                    <div class="col-12 pt-2">
-                        <a href="{{url('redirect/facebook')}}" class="btn btn-primary w-100 py-2" style="background:#3c5a99;"><b>Login with Facebook</b></a>
-                    </div>
+                    
                 </div>
             </div>
         </div>
     </div>
 
-@endsection
