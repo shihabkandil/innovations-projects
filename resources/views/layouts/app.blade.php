290e76cc6d61b9eedd724cec42339f07a5ceeb19
Fady1900456 <fady1900456@miuegypt.edu.eg>
2022-04-02 23:06:19 +0200

    fixed dropdown menu and chat ui

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -29,6 +29,107 @@
     <!-- Template Stylesheet -->
     <link href="css/style.css" rel="stylesheet">
 
+    <style>
+        .open-button {
+            background-color: #FFC107;
+            color: white;
+            padding: 13px 20px;
+            border: none;
+            cursor: pointer;
+            position: fixed;
+            bottom: 44px;
+            right: 110px;
+            z-index: 10;
+        }
+
+        .close-button {
+            display: none;
+            background-color: #FFC107;
+            color: white;
+            padding: 13px 20px;
+            border: none;
+            cursor: pointer;
+            position: fixed;
+            bottom: 44px;
+            right: 110px;
+            z-index: 10;
+        }
+
+        .send-button {
+        background-color: #FFC107;
+        color: white;
+        padding: 5px 20px;
+        border-radius: 15px;
+        border: none;
+        margin: 5px;
+        margin-left: auto;
+        cursor: pointer;
+        }
+
+        #chatBox{
+            display: none;
+            position: fixed;
+            bottom: 130px;
+            right: 110px;
+            z-index: 10;
+        }
+
+        #chatBox body {
+            background: #000000;
+        }
+
+        #chatBox .card {
+            width: 300px;
+            border: none;
+            border-radius: 15px;
+        }
+
+        #chatBox .adiv {
+            background: #FFC107;
+            border-radius: 15px;
+            border-bottom-right-radius: 0;
+            border-bottom-left-radius: 0;
+            font-size: 20px;
+            height: 46px;
+        }
+
+        #chatBox .chat {
+            border: none;
+            background: #E2FFE8;
+            font-size: 13px;
+            border-radius: 20px;
+        }
+
+        #chatBox .bg-white {
+            border: 1px solid #E7E7E9;
+            font-size: 13px;
+            border-radius: 20px;
+        }
+
+        #chatBox .myvideo img {
+            border-radius: 20px;
+        }
+
+        #chatBox .dot {
+            font-weight: bold;
+        }
+
+        #chatBox .form-control {
+            border-radius: 12px;
+            border: 1px solid #F0F0F0;
+            font-size: 12px;
+        }
+
+        #chatBox .form-control:focus {
+            box-shadow: none;
+        }
+
+        #chatBox .form-control::placeholder {
+            font-size: 8px;
+            color: #C4C4C4;
+        }
+</style>
+
 </head>
 
 
@@ -63,8 +164,8 @@
                 @auth
                 <!-- Navbar-->
                 <ul class="navbar-nav ms-auto ms-md-0 me-3 me-lg-4">
-                    <li class="nav-item dropdown">
-                        <a class="nav-link dropdown-toggle" id="navbarDropdown" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false"><i class="fas fa-user fa-fw"></i></a>
+                    <li class="nav-item dropdown-menu-right">
+                        <a class="nav-link dropdown-toggle dropleft" id="navbarDropdown" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false"><i class="fas fa-user fa-fw"></i></a>
                         <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
                             <li><a class="dropdown-item" href="/editProfile">Edit Profile</a></li>
                             <li><a class="dropdown-item" href="/settings">Settings</a></li>
@@ -73,6 +174,27 @@
                         </ul>
                     </li>
                 </ul>
+
+                <div id="chatBox">
+                    <div class="container d-flex justify-content-center">
+                        <div class="card mt-5">
+                            <div class="d-flex flex-row justify-content-between p-3 adiv text-white"> <span class="pb-3 mx-auto">Live chat</span> </div>
+                            <div class="d-flex flex-row p-3"> <img src="https://img.icons8.com/color/48/000000/circled-user-female-skin-type-7.png" width="30" height="30">
+                                <div class="chat ml-2 p-3">Hello</div>
+                            </div>
+                            <div class="d-flex flex-row p-3" style="margin-left: auto;">
+                                <div class="bg-white mr-2 p-3"><span class="text-muted">Hello</span></div> <img src="https://img.icons8.com/color/48/000000/circled-user-male-skin-type-7.png" width="30" height="30">
+                            </div>
+                            <div class="d-flex flex-row p-3"> <img src="https://img.icons8.com/color/48/000000/circled-user-female-skin-type-7.png" width="30" height="30">
+                                <div class="chat ml-2 p-3"><span class="text-muted dot">. . .</span></div>
+                            </div>
+                            <div class="form-group px-3"> <textarea class="form-control" style="resize: none;" rows="5" placeholder="Type your message"></textarea> </div>
+                            <button type="button" class="send-button">Send</button>
+                        </div>
+                    </div>
+                </div>
+                <button class="close-button" onclick="closeForm()" >Close</button>
+                <button class="open-button" onclick="openForm()">Chat</button>
                 @endauth
 
             @guest
@@ -133,27 +255,6 @@
     <!-- Back to Top -->
     <a href="#" class="btn btn-lg btn-primary btn-lg-square back-to-top"><i class="bi bi-arrow-up"></i></a>
 
-    <div id="chatBox">
-        <div class="container d-flex justify-content-center">
-            <div class="card mt-5">
-                <div class="d-flex flex-row justify-content-between p-3 adiv text-white"> <span class="pb-3 mx-auto">Live chat</span> </div>
-                <div class="d-flex flex-row p-3"> <img src="https://img.icons8.com/color/48/000000/circled-user-female-skin-type-7.png" width="30" height="30">
-                    <div class="chat ml-2 p-3">Hello</div>
-                </div>
-                <div class="d-flex flex-row p-3" style="margin-left: auto;">
-                    <div class="bg-white mr-2 p-3"><span class="text-muted">Hello</span></div> <img src="https://img.icons8.com/color/48/000000/circled-user-male-skin-type-7.png" width="30" height="30">
-                </div>
-                <div class="d-flex flex-row p-3"> <img src="https://img.icons8.com/color/48/000000/circled-user-female-skin-type-7.png" width="30" height="30">
-                    <div class="chat ml-2 p-3"><span class="text-muted dot">. . .</span></div>
-                </div>
-                <div class="form-group px-3"> <textarea class="form-control" style="resize: none;" rows="5" placeholder="Type your message"></textarea> </div>
-                <button type="button" class="send-button">Send</button>
-            </div>
-        </div>
-    </div>
-    <button class="close-button" onclick="closeForm()">Close</button>
-    <button class="open-button" onclick="openForm()">Chat</button>
-
     <!-- JavaScript Libraries -->
     <script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/js/bootstrap.bundle.min.js"></script>
@@ -180,105 +281,5 @@
         document.getElementsByClassName("close-button")[0].style.display = "none";
     }
 </script>
-<style>
-.open-button {
-    background-color: #FFC107;
-    color: white;
-    padding: 13px 20px;
-    border: none;
-    cursor: pointer;
-    position: fixed;
-    bottom: 44px;
-    right: 110px;
-    z-index: 10;
-}
-
-.close-button {
-    display: none;
-    background-color: #FFC107;
-    color: white;
-    padding: 13px 20px;
-    border: none;
-    cursor: pointer;
-    position: fixed;
-    bottom: 44px;
-    right: 110px;
-    z-index: 10;
-}
-
-.send-button {
-  background-color: #FFC107;
-  color: white;
-  padding: 5px 20px;
-  border-radius: 15px;
-  border: none;
-  margin: 5px;
-  margin-left: auto;
-  cursor: pointer;
-}
-
-#chatBox{
-    display: none;
-    position: fixed;
-    bottom: 130px;
-    right: 110px;
-    z-index: 10;
-}
-
-#chatBox body {
-    background: #000000;
-}
-
-#chatBox .card {
-    width: 300px;
-    border: none;
-    border-radius: 15px;
-}
-
-#chatBox .adiv {
-    background: #FFC107;
-    border-radius: 15px;
-    border-bottom-right-radius: 0;
-    border-bottom-left-radius: 0;
-    font-size: 20px;
-    height: 46px;
-}
-
-#chatBox .chat {
-    border: none;
-    background: #E2FFE8;
-    font-size: 13px;
-    border-radius: 20px;
-}
-
-#chatBox .bg-white {
-    border: 1px solid #E7E7E9;
-    font-size: 13px;
-    border-radius: 20px;
-}
-
-#chatBox .myvideo img {
-    border-radius: 20px;
-}
-
-#chatBox .dot {
-    font-weight: bold;
-}
-
-#chatBox .form-control {
-    border-radius: 12px;
-    border: 1px solid #F0F0F0;
-    font-size: 12px;
-}
-
-#chatBox .form-control:focus {
-    box-shadow: none;
-}
-
-#chatBox .form-control::placeholder {
-    font-size: 8px;
-    color: #C4C4C4;
-}
-</style>
 </html>
 
