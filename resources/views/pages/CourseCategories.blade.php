e0a9dbadd80ff56f442d578d49155d24229ceb6b
Shihab Kandil <jupitar.orbit01@gmail.com>
2022-04-04 03:26:28 +0200

    UI enhancements

--- resources/views/pages/CourseCategories.blade.php
+++ resources/views/pages/CourseCategories.blade.php
@@ -5,10 +5,17 @@
 @section('content')
 
     @foreach($courses as $course)
-         
-               
-     <p>{{ $course->CourseName }}</p>
-                          
+    
+    <div class="col-lg-4 col-md-6 wow fadeInUp" data-wow-delay="0.1s">
+        <div class="course-item bg-light">
+            <div class="position-relative overflow-hidden">
+                <a href="/course/{{$course->id}}"><img class="img-fluid" src={{ asset('img/'.$course->CoursePicture)}}></a>
+            </div>
+            <div class="text-center p-4 pb-0">
+                <h5 class="mb-4">{{$course->CourseName}}</h5>
+            </div>
+        </div>
+    </div>
      
     @endforeach
 
--- resources/views/pages/browseCategory.blade.php
+++ resources/views/pages/browseCategory.blade.php
@@ -4,44 +4,42 @@
 
 @section('content')
 <br>
-<div class="row g-4 justify-content-center">
-	<div id="categoryAjax">
+<div class="row g-4 justify-content-center categories">
 
-	</div>
 </div>
 
 <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js" integrity="sha512-894YE6QWD5I59HgZOGReFYm4dnWc1Qt5NtvYSaNcOP+u1T9qYdvdihz0PPSiiqn/+/3e7Jo4EaG7TubfWGUrMQ==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
 <script>
-	$(document).ready(function(){
-		
-		setInterval(function(){ 
-			fetchCategory();
-		}, 10000);
+    $(document).ready(function(){
 
-		function fetchCategory(){
-			$.ajax({
-				type: "GET",
-				url: "/fetch-categories",
-				dataType: "json",
-				success: function(response){
-					$('#categoryAjax').html("");
-					$.each(response.categories, function(key, item){
-						$('#categoryAjax').append(
-							'<div class="col-lg-4 col-md-6 wow fadeInUp" data-wow-delay="0.1s">\
-								<div class="course-item bg-light">\
-									<div class="position-relative overflow-hidden">\
-										<a href="Categories/'+item.id+'"><img class="img-fluid" src="img/'+item.picture+'" alt="'+item.name+'"></a>\
-									</div>\
-									<div class="text-center p-4 pb-0">\
-										<h5 class="mb-4">'+item.name+'</h5>\
-									</div>\
-								</div>\
-							</div>');
-					});
-				}
-			});
-		}
-	});
+         setInterval(function(){ 
+             fetchCategory();
+         }, 10000);
+
+        function fetchCategory(){
+            $.ajax({
+                type: "GET",
+                url: "/fetch-categories",
+                dataType: "json",
+                success: function(response){
+                    $('.categories').html("");
+                    $.each(response.categories, function(key, item){
+                        $('.categories').append(
+                            '<div class="col-lg-4 col-md-6 wow fadeInUp" data-wow-delay="0.1s">\
+                                <div class="course-item bg-light">\
+                                    <div class="position-relative overflow-hidden">\
+                                        <a href="Categories/'+item.id+'"><img class="img-fluid" src="img/'+item.picture+'" alt="'+item.name+'"></a>\
+                                    </div>\
+                                    <div class="text-center p-4 pb-0">\
+                                        <h5 class="mb-4">'+item.name+'</h5>\
+                                    </div>\
+                                </div>\
+                            </div>');
+                    });
+                }
+            });
+        }
+    });
 </script>
 
 @endsection
