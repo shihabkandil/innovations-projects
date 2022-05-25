324889ffa626705f9fac5542bd6cc19615f94672
Fady1900456 <fady1900456@miuegypt.edu.eg>
2022-05-25 20:38:58 +0200

    removed footer information

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -154,27 +154,6 @@
     <!-- Footer Start -->
     <div class="container-fluid bg-dark text-light footer pt-5 mt-5 wow fadeIn" data-wow-delay="0.1s" dir="ltr">
         <div class="container py-5">
-            <div class="row g-5">
-                <div class="col-lg-3 col-md-6">
-                    <h4 class="text-white mb-3">{{ __('Quick Link') }}</h4>
-                    <a class="btn btn-link" href="/about">{{ __('About Us') }}</a>
-                    <a class="btn btn-link" href="/contact">{{ __('Contact Us') }}</a>
-                    <a class="btn btn-link" href="/privacy-policy">{{ __('Privacy Policy') }}</a>
-                    <a class="btn btn-link" href="/terms-conditions">{{ __('Terms & Conditions') }}</a>
-                </div>
-                <div class="col-lg-3 col-md-6">
-                    <h4 class="text-white mb-3">{{ __('Contact') }}</h4>
-                    <p class="mb-2"><i class="fa fa-map-marker-alt me-3"></i>123 Street, New York, USA</p>
-                    <p class="mb-2"><i class="fa fa-phone-alt me-3"></i>+012 345 67890</p>
-                    <p class="mb-2"><i class="fa fa-envelope me-3"></i>info@example.com</p>
-                    <div class="d-flex pt-2">
-                        <a class="btn btn-outline-light btn-social" href=""><i class="fab fa-twitter"></i></a>
-                        <a class="btn btn-outline-light btn-social" href=""><i class="fab fa-facebook-f"></i></a>
-                        <a class="btn btn-outline-light btn-social" href=""><i class="fab fa-youtube"></i></a>
-                        <a class="btn btn-outline-light btn-social" href=""><i class="fab fa-linkedin-in"></i></a>
-                    </div>
-                </div>
-            </div>
         </div>
         <div class="container">
             <div class="copyright">
