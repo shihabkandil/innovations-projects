74123ab2a04a46f187cf67ca1f1a343e75ec73c5
bilal-tamer <bilal.ghareeb@gmail.com>
2022-05-25 21:16:07 +0200

    create admin migration and delete users migration with moving index method to admincontroller

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -7,6 +7,17 @@ use App\Models\ContentCreator;
 
 class AdminController extends Controller
 {
+        /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return view('pages.AdminLogin');
+    }
+
+    
     public function viewContentCreators(){
         return view('admin.viewContentCreators',['contentCreators' => ContentCreator::getall()]);
     }
