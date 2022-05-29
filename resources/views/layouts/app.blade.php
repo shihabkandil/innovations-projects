78e7e98ce283dea022eb083431487edbfbac9d38
Fady1900456 <fady1900456@miuegypt.edu.eg>
2022-05-30 00:01:43 +0200

    translations

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -61,11 +61,11 @@
                     <li class="nav-item dropdown">
                         <a class="nav-link dropdown-toggle" id="navbarDropdown" href="#" role="button" style="color:red; font-weight:bold;" data-bs-toggle="dropdown" aria-expanded="false">{{ __('Request') }}</a>
                         <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
-                            <li><a href="contentCreator/addCourse" class="dropdown-item">{{ __('messages.Add Course') }}</a></li>
+                            <li><a href="/contentCreator/addCourse" class="dropdown-item">{{ __('messages.Add Course') }}</a></li>
                             <li><hr class="dropdown-divider"></li>
-                            <li><a href="contentCreator/addContentRequest" class="dropdown-item">{{ __('messages.Adding Content') }}</a></li>
+                            <li><a href="/contentCreator/addContentRequest" class="dropdown-item">{{ __('messages.Adding Content') }}</a></li>
                             <li><hr class="dropdown-divider"></li>
-                            <li><a href="contentCreator/writeArticle" class="dropdown-item">{{ __('Write an Article') }}</a></li>
+                            <li><a href="/contentCreator/writeArticle" class="dropdown-item">{{ __('messages.Write an Article') }}</a></li>
                         </ul>
                     </li>
                 </ul>
@@ -100,7 +100,11 @@
                 <ul class="navbar-nav ms-auto ms-md-0 me-3 me-lg-4">
                     <li class="nav-item dropdown">
                         <a class="nav-link dropdown-toggle" id="navbarDropdown" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false"><i class="fas fa-user fa-fw"></i></a>
+                        @if(App::getLocale() == 'en')
                         <ul class="dropdown-menu" style="right: 0; left: auto;" aria-labelledby="navbarDropdown">
+                        @else
+                        <ul class="dropdown-menu" style="right: auto; left: 0;" aria-labelledby="navbarDropdown">
+                        @endif
                             @if(Auth::guard('student')->check())
                             <li><a class="dropdown-item" href="/editProfile/{{Auth::guard('student')->user()->id}}">Edit Profile</a></li>
                             <li><hr class="dropdown-divider" /></li>
