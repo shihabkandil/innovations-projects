fe0fa0f12df088df74932fd7d8a331499862eeb1
Fady1900456 <fady1900456@miuegypt.edu.eg>
2022-05-30 00:03:10 +0200

    coursebuilder input validation

--- app/Http/Controllers/ContentCreatorController.php
+++ app/Http/Controllers/ContentCreatorController.php
@@ -34,12 +34,29 @@ class ContentCreatorController extends Controller
     }
 
     public function submitCourse(Request $request){
+        $request->validate([
+            'courseName' => ['required', 'string', 'max:255'],
+            'courseCategory' => ['required','integer'],
+            'coursePrice' => ['required','integer'],
+            'coursePicture'=> ['required','mimes:jpeg,jpg,png,gif|required|max:10000'],
+            'courseDuration' => ['required','integer'],
+            'duration' => ['required', 'string', 'max:255'],
+            'certificate' => ['required', 'string', 'max:255'],
+            'language' => ['required', 'string', 'max:255'],
+            'skills' => ['required', 'string', 'max:255'],
+            'courseDescription' => ['required','string'],
+            'whatWillILearn' => ['required','string'],
+            'learningOutcomes' => ['required','string'],
+        ]);
+
+        $picture = FirestorageController::store($request['coursePicture'], 'Courses/Pictures/');
+
         $course = Courses::create([
             'CourseName' => $request['courseName'],
             'CategoryID' => $request['courseCategory'],
             'CoursePrice' => $request['coursePrice'],
             'CourseInstructorID' => $request['currentUserID'],
-            'CoursePicture' => $request['coursePicture'],
+            'CoursePicture' => $picture,
             'duration' => $request['courseDuration'] . $request['duration'],
             'certificate' => $request['certificate'],
             'lang' => $request['language'],
