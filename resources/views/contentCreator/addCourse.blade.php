c2bbdede8e6845a516655008ea3c4bf00b3b2917
Fady1900456 <fady1900456@miuegypt.edu.eg>
2022-06-02 18:45:13 +0200

    ui changes

--- resources/views/contentCreator/addCourse.blade.php
+++ resources/views/contentCreator/addCourse.blade.php
@@ -14,8 +14,6 @@
             <div class="position-relative overflow-hidden">
                 <img class="img-fluid" src="<?php echo FirestorageController::fetch('Courses/Pictures/',$data->CoursePicture)?>" alt="{{$data->CourseName}} picture" width="100%">
                 <div class="w-100 d-flex justify-content-center position-absolute bottom-0 start-0 mb-4">
-                    <a href="#" class="flex-shrink-0 btn btn-sm btn-primary px-3 border-end" style="border-radius: 30px 0 0 30px;">Read More</a>
-                    <a href="#" class="flex-shrink-0 btn btn-sm btn-primary px-3" style="border-radius: 0 30px 30px 0;">Join Now</a>
                 </div>
             </div>
             <div class="text-center p-4 pb-0">
--- resources/views/pages/CourseCategories.blade.php
+++ resources/views/pages/CourseCategories.blade.php
@@ -31,8 +31,6 @@
             <div class="position-relative overflow-hidden">
                 <img class="img-fluid" src="<?php echo FirestorageController::fetch('Courses/Picture/',$data->CoursePicture)?>" alt="{{$data->CourseName}}">
                 <div class="w-100 d-flex justify-content-center position-absolute bottom-0 start-0 mb-4">
-                    <a href="/course/{{$data->id}}" class="flex-shrink-0 btn btn-sm btn-primary px-3 border-end" style="border-radius: 30px 0 0 30px;">Read More</a>
-                    <a href="/cart" class="flex-shrink-0 btn btn-sm btn-primary px-3" style="border-radius: 0 30px 30px 0;">Join Now</a>
                 </div>
             </div>
             <div class="text-center p-4 pb-0">
