15e10fd6211bbcd555e10dd030945d368c141069
Shihab Kandil <jupitar.orbit01@gmail.com>
2022-06-04 18:38:37 +0200

    id changes

--- database/migrations/2022_03_30_171328_create_courses_table.php
+++ database/migrations/2022_03_30_171328_create_courses_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
     public function up()
     {
         Schema::create('courses', function (Blueprint $table) {
-            $table->id('CourseID');
+            $table->id();
             $table->string('CourseName')->unique();
             $table->integer('CategoryID');
             $table->double('CoursePrice')->unique();
